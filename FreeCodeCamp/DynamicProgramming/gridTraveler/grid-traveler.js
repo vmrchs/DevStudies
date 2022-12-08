3140da3903da712f67296f0115d562a0dc74ffe9
Vinnie Marchisio <vmrchs@gmail.com>
2022-12-08 13:10:44 -0300

    trying to understand better the grid-traveler exercise

--- FreeCodeCamp/DynamicProgramming/gridTraveler/grid-traveler.js
+++ FreeCodeCamp/DynamicProgramming/gridTraveler/grid-traveler.js
@@ -1,15 +1,21 @@
 const gridTraveler = (m, n, memo = {}) => {
   const key = m + "," + n;
-  // are the arguments in the memo
+  console.log(memo);
+  console.log(memo[key]);
+  // check if the arguments are already in the memo
   if (key in memo) return memo[key];
-  if (m === 1 && n === 1) return 1;
-  if (m === 0 || n === 0) return 0;
-  memo[key] = gridTraveler(m - 1, n, memo) + gridTraveler(m, n - 1, memo);
+  // check if it is a basic case
+  if (m === 1 && n === 1) return 1; // valid path
+  if (m === 0 || n === 0) return 0; // invalid path
+  memo[key] = gridTraveler(m - 1, n, memo) + gridTraveler(m, n - 1, memo); // return the sum of me going down, and going right
+  // console.log(memo);
   return memo[key];
 };
 
-console.log(gridTraveler(1, 1));
-console.log(gridTraveler(2, 3));
-console.log(gridTraveler(3, 2));
-console.log(gridTraveler(3, 3));
-console.log(gridTraveler(18, 18));
+// console.log(gridTraveler(1, 1)); // 1
+// console.log(gridTraveler(2, 3)); // 3
+// console.log(gridTraveler(3, 2)); // 3
+// console.log(gridTraveler(3, 3)); // 6
+// console.log(gridTraveler(18, 18)); // 2333606220
+
+console.log(gridTraveler(2, 3)); // 3
