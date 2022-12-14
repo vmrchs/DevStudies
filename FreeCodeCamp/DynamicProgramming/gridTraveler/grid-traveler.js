8870926c649ff1ea968e1c1ac463f6b4372ea0eb
Vinnie Marchisio <vmrchs@gmail.com>
2022-12-14 16:00:57 -0300

    update on the FreeCodeCamp folder, DynamicProgramming course

--- FreeCodeCamp/DynamicProgramming/gridTraveler/grid-traveler.js
+++ FreeCodeCamp/DynamicProgramming/gridTraveler/grid-traveler.js
@@ -1,14 +1,17 @@
 const gridTraveler = (m, n, memo = {}) => {
   const key = m + "," + n;
-  console.log(memo);
-  console.log(memo[key]);
+  // console.log(memo) will bring the memo object, with all its items
+  // console.log(memo);
+
+  // memo[key] will then bring the sum of 1 values found through the recursion
+  // console.log(memo[key]);
+
   // check if the arguments are already in the memo
   if (key in memo) return memo[key];
   // check if it is a basic case
   if (m === 1 && n === 1) return 1; // valid path
   if (m === 0 || n === 0) return 0; // invalid path
   memo[key] = gridTraveler(m - 1, n, memo) + gridTraveler(m, n - 1, memo); // return the sum of me going down, and going right
-  // console.log(memo);
   return memo[key];
 };
 
