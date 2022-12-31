42fb3d3554fc7ddca614336f692986d269d9d236
Vinnie Marchisio <vmrchs@gmail.com>
2022-12-31 12:24:00 -0300

    finished the last dynamic programming exercise

--- FreeCodeCamp/DynamicProgramming/allConstructTabulation/allConstructTabulation.js
+++ FreeCodeCamp/DynamicProgramming/allConstructTabulation/allConstructTabulation.js
@@ -1,5 +1,20 @@
 const allConstruct = (target, wordBank) => {
-  const table = Array(target.length + 1).fill([]);
+  const table = Array(target.length + 1)
+    .fill()
+    .map(() => []);
+
+  table[0] = [[]];
+
+  for (let i = 0; i <= target.length; i++) {
+    for (let word of wordBank) {
+      if (target.slice(i, i + word.length) === word) {
+        const newCombinations = table[i].map((subArray) => [...subArray, word]);
+        table[i + word.length].push(...newCombinations);
+      }
+    }
+  }
+
+  return table[target.length];
 };
 
 console.log(allConstruct("purple", ["purp", "p", "ur", "le", "purpl"]));
@@ -20,13 +35,5 @@ console.log(
   allConstruct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"])
 );
 // []
-console.log(
-  allConstruct("aaaaaaaaaaaaaaaaaaaaaaaaaaz", [
-    "a",
-    "aa",
-    "aaa",
-    "aaaa",
-    "aaaaa",
-  ])
-);
+console.log(allConstruct("aaaaaaaaaaz", ["a", "aa", "aaa", "aaaa", "aaaaa"]));
 // []
