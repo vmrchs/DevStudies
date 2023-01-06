07f84b15c90b80356bd75753677b8b9706caa346
Vinnie Marchisio <vmrchs@gmail.com>
2023-01-06 18:23:14 -0300

    DOM Manipulation Course - Part 2. Project 2 - Modal.

--- JavaScriptDOMManipulation/project1QuoteGenerator/project-1.js
+++ JavaScriptDOMManipulation/project1QuoteGenerator/project-1.js
@@ -1144,7 +1144,3 @@ btn.addEventListener("click", function () {
   quote.innerText = quotes[random].quote;
   person.innerText = quotes[random].person;
 });
-
-// Functions
-
-function newQuote() {}
